c3fa7ebf8d353b66940b75414e38d209e38d4ff0
Andreas Heissenberger <andreas@heissenberger.at>
2024-12-19 17:13:29 +0100

    fix: make it simpler

---
+++ src/s/page/hello.jsx
@@ -0,0 +1,9 @@
+import { headers } from "@lazarv/react-server";
+
+export default async function HelloPage() {
+  headers({
+    "cache-control": "s-maxage=1",
+  });
+  return (<div>Hello</div>)
+
+}
